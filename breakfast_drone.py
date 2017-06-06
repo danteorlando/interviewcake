5619859fe36edeb5209dd1d647b1b5d34bd56cf9
danteorlando <danteorlando@users.noreply.github.com>
2017-06-06 10:58:18 -0700

    updated test case

--- breakfast_drone.py
+++ breakfast_drone.py
@@ -10,6 +10,7 @@ def find_missing_drone(delivery_id_confirmations):
 def test():
     assert find_missing_drone([1,2,3,4,5,1,2,3,4]) == 5
     assert find_missing_drone([21,33,21,45,78,89,45,33,89]) == 78
+    assert find_missing_drone([21,33,21,45,78,89,45,33,89,78]) == 0
     
     
 test()
