0324ac6d9a7a64bf92fda3e8da9c9ab73693406f
danteorlando <danteorlando@users.noreply.github.com>
2017-07-21 15:51:20 -0700

    update

--- single_riffle.py
+++ single_riffle.py
@@ -3,53 +3,53 @@
 I figured out how to get rich: online poker.
 I suspect the online poker game I'm playing shuffles cards by doing a single "riffle"
 
-To prove this, let's write a function to tell us if a full deck of cards shuffled_deck is a 
+To prove this, let's write a function to tell us if a full deck of cards shuffled_deck is a
 single riffle of two other halves half1 and half2.
 
 We'll represent a stack of cards as a list of integers in the range 1..52 (since there are 52 distinct cards in a deck).
 
-Why do I care? A single riffle is not a completely random shuffle. If I'm right, I can make more informed bets and get rich 
-and finally prove to my ex that I am not a "loser with an unhealthy cake obsession" (even though it's too late now because 
+Why do I care? A single riffle is not a completely random shuffle. If I'm right, I can make more informed bets and get rich
+and finally prove to my ex that I am not a "loser with an unhealthy cake obsession" (even though it's too late now because
 she let me go and she's never getting me back).
 
 Breakdown
 How can we re-phrase this problem in terms of smaller subproblems?
 
-Breaking the problem into smaller subproblems will clearly involve reducing the size of at least one of our stacks of cards. 
+Breaking the problem into smaller subproblems will clearly involve reducing the size of at least one of our stacks of cards.
 So to start, let's try taking the first card out of shuffled_deck.
 
 What should be true of this card if shuffled_deck is a riffle of half1 and half2?
 
 
-If shuffled_deck is a riffle of half1 and half2, then the first card from shuffled_deck should be either the same as the 
+If shuffled_deck is a riffle of half1 and half2, then the first card from shuffled_deck should be either the same as the
 first card from half1 or the same as the first card from half2.
 
 If we're looking at our decks face-up, from above (so we only see the top card), this could be a single riffle:
 
-Looking at three face up stacks of cards, the top card of the shuffled deck is 10, the top card of half 1 is 7, 
+Looking at three face up stacks of cards, the top card of the shuffled deck is 10, the top card of half 1 is 7,
 and the top card of half 2 is 10.
 
 While this could not:
 
-Looking at three face up stacks of cards, the top card of the shuffled deck is 32, the top card of half 1 is 7, 
+Looking at three face up stacks of cards, the top card of the shuffled deck is 32, the top card of half 1 is 7,
 and the top card of half 2 is 10.
 
 Now that we know the first card checks out, how do we get to our subproblem?
 
 
-Let's "throw out" the top card from shuffled_deck as well as the card it matched with from the top of half1 or half2. 
+Let's "throw out" the top card from shuffled_deck as well as the card it matched with from the top of half1 or half2.
 Those cards are now "accounted for."
 
-Now we're left with a smaller version of the original problem, which we can solve using the same approach! 
-So we keep doing this over and over until we exhaust shuffled_deck. 
+Now we're left with a smaller version of the original problem, which we can solve using the same approach!
+So we keep doing this over and over until we exhaust shuffled_deck.
 If we get to the end and each card "checks out," we return True.
 
 How do we implement this in code?
 
-Now that we have a problem that's the same as the original problem except smaller, our first thought might be to use recursion. 
+Now that we have a problem that's the same as the original problem except smaller, our first thought might be to use recursion.
 All we need is a base case. What's our base case?
 
-We stop when we run out of cards in our shuffled_deck. So that's our base case: when we've checked all cards in shuffled_deck, 
+We stop when we run out of cards in our shuffled_deck. So that's our base case: when we've checked all cards in shuffled_deck,
 we return True because we know all of the cards have been "accounted for."
 
   def is_single_riffle(half1, half2, shuffled_deck):
@@ -79,12 +79,12 @@ we return True because we know all of the cards have been "accounted for."
 
 This solution will work. But we can do better.
 
-Before we talk about optimization, note that our inputs are of small and constant size. 
-This function will take hardly any time or space, even if it could be more efficient. 
-In industry, especially at small startups that want to move quickly, optimizing this might be considered a "premature optimization." 
-But if we're going to do something inefficient, we should at least know about it. Great engineers have both the skill to see how 
-to optimize their code and the wisdom to know when those optimizations aren't worth it. At this point in the interview I recommend 
-saying "I think we can optimize this a bit further, although given the constraints on the input this probably won't be very 
+Before we talk about optimization, note that our inputs are of small and constant size.
+This function will take hardly any time or space, even if it could be more efficient.
+In industry, especially at small startups that want to move quickly, optimizing this might be considered a "premature optimization."
+But if we're going to do something inefficient, we should at least know about it. Great engineers have both the skill to see how
+to optimize their code and the wisdom to know when those optimizations aren't worth it. At this point in the interview I recommend
+saying "I think we can optimize this a bit further, although given the constraints on the input this probably won't be very
 resource-intensive anyway...should we talk about optimizations?"
 
 Okay, back to our show. This function will take O(n^2) time and O(n^2) additional space.
@@ -97,24 +97,24 @@ In particular this expression:
 
   half1[1:]
 
-That's a slice, and it costs O(m) time and space, where m is the size of the resulting list. That's going to determine our overall 
+That's a slice, and it costs O(m) time and space, where m is the size of the resulting list. That's going to determine our overall
 time and space cost here the rest of the work we're doing is constant space and time.
 
-In our recursing we'll build up n frames on the call stack. Each of those frames will hold a different slice of our original shuffled_deck 
+In our recursing we'll build up n frames on the call stack. Each of those frames will hold a different slice of our original shuffled_deck
 (and half1 and half2, though we only slice one of them in each recursive call).
 
 So, what's the total time and space cost of all our slices?
 
-If shuffled_deck has n items to start, taking our first slice takes n-1 time and space (plus half that, since we're also slicing one of our 
+If shuffled_deck has n items to start, taking our first slice takes n-1 time and space (plus half that, since we're also slicing one of our
 halves, but that's just a constant multiplier so we can ignore it). In our second recursive call, slicing takes n-2 time and space. Etcetera.
 
 So our total time and space cost for slicing comes to:
 
-(n - 1) + (n - 2) + ... + 2 + 1 
+(n - 1) + (n - 2) + ... + 2 + 1
 
 This is a common series that turns out to be O(n^2).
 
-We can do better than this O(n^2) time and space cost. 
+We can do better than this O(n^2) time and space cost.
 One way we could to that is to avoid slicing and instead keep track of indices in the list:
 
   def is_single_riffle(half1, half2, shuffled_deck, shuffled_deck_index=0, half1_index=0, half2_index=0):
@@ -149,23 +149,23 @@ One way we could to that is to avoid slicing and instead keep track of indices i
         half1, half2, shuffled_deck, shuffled_deck_index, half1_index, half2_index
     )
 
-So now we're down to O(n) time, but we're still taking O(n) space in the call stack because of our recursion. 
+So now we're down to O(n) time, but we're still taking O(n) space in the call stack because of our recursion.
 We can rewrite this as an iterative function to get that memory cost down to O(1).
 
-What's happening in each iteration of our recursive function? Sometimes we're taking a card off of half1 
+What's happening in each iteration of our recursive function? Sometimes we're taking a card off of half1
 and sometimes we're taking a card off of half2, but we're always taking a card off of shuffled_deck.
 
 So what if instead of taking cards off of shuffled_deck 1-by-1, we iterated over them?
 
 Solution
-We walk through shuffled_deck, seeing if each card so far could have come from a riffle of the other two halves. 
+We walk through shuffled_deck, seeing if each card so far could have come from a riffle of the other two halves.
 
 To check this, we:
 
 Keep pointers to the current index in half1, half2, and shuffled_deck.
 Walk through shuffled_deck from beginning to end.
-If the current card in shuffled_deck is the same as the top card from half1, increment half1_index and keep going. 
-This can be thought of as "throwing out" the top cards of half1 and shuffled_deck, reducing the problem to the 
+If the current card in shuffled_deck is the same as the top card from half1, increment half1_index and keep going.
+This can be thought of as "throwing out" the top cards of half1 and shuffled_deck, reducing the problem to the
 remaining cards in the stacks.
 
 Same as above with half2.
@@ -206,22 +206,22 @@ If we make it all the way to the end of shuffled_deck and each card checks out, 
 Complexity
 O(n) time and O(1) additional space.
 
-Becky if you're reading this I didn't really mean what I said in the problem statement. It's just that things have 
-been hard lately and anyway if you'll just give me another chance I promise it won't be like last time. 
+Becky if you're reading this I didn't really mean what I said in the problem statement. It's just that things have
+been hard lately and anyway if you'll just give me another chance I promise it won't be like last time.
 I'm a wreck without you. Like a collapsed souffle. Please Becky.
 
 Bonus
 This assumes shuffled_deck contains all 52 cards. What if we can't trust this (e.g. some cards are being secretly removed by the shuffle)?
 This assumes each number in shuffled_deck is unique. How can we adapt this to rifling lists of random integers with potential repeats?
-Our solution returns True if you just cut the deck take one half and put it on top of the other. While that technically meets the 
+Our solution returns True if you just cut the deck take one half and put it on top of the other. While that technically meets the
 definition of a riffle, what if you wanted to ensure that some mixing of the two halves occurred?
 
 What We Learned
-If you read the whole breakdown section, you might have noticed that we started with a recursive function that looked simple but 
-turned out to cost O(n^2) time and space because each recursive step created its own new "slice" of the input list. 
+If you read the whole breakdown section, you might have noticed that we started with a recursive function that looked simple but
+turned out to cost O(n^2) time and space because each recursive step created its own new "slice" of the input list.
 If you missed that part, go back and take a look.
 
-Be careful of the hidden time and space costs of list slicing! Consider tracking list indices "by hand" instead 
+Be careful of the hidden time and space costs of list slicing! Consider tracking list indices "by hand" instead
 (as we do in our final solution).
 
 '''
@@ -242,14 +242,7 @@ def is_single_riffle(h1, h2, d):
             ctr2 += 1
         else:
             return False
-        
+
     return True
 
 print(is_single_riffle(half1, half2, deck))
-
-
-
-
-
-
-
