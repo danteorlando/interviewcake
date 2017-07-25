136e19c7a9dceb84d650d5475be5a6b63bde80cc
danteorlando <danteorlando@users.noreply.github.com>
2017-07-24 19:06:15 -0700

    added class

--- second_largest_element_in_bst.py
+++ second_largest_element_in_bst.py
@@ -234,6 +234,20 @@ This'll keep your thinking organized and also help get your interviewer on
 the same page.
 
 '''
+class BinaryTreeNode:
 
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+        
 def second_largest(root):
     return
