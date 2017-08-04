cccb509a1ede2df431371364a09d5094af769b29
danteorlando <danteorlando@users.noreply.github.com>
2017-08-04 11:03:45 -0700

    update text

--- breakfast_drone.py
+++ breakfast_drone.py
@@ -1,19 +1,23 @@
 """
-Your company delivers breakfast via autonomous quadcopter drones. And something mysterious has happened.
+Your company delivers breakfast via autonomous quadcopter drones. And something
+mysterious has happened.
 
-Each breakfast delivery is assigned a unique ID, a positive integer. When one of the company's 100 drones 
-takes off with a delivery, the delivery's ID is added to a list, delivery_id_confirmations. When the drone 
-comes back and lands, the ID is again added to the same list.
+Each breakfast delivery is assigned a unique ID, a positive integer. When one of
+the company's 100 drones takes off with a delivery, the delivery's ID is added
+to a list, delivery_id_confirmations. When the drone comes back and lands, the
+ID is again added to the same list.
 
-After breakfast this morning there were only 99 drones on the tarmac. One of the drones never made it back 
-from a delivery. We suspect a secret agent from Amazon placed an order and stole one of our patented 
-drones. To track them down, we need to find their delivery ID.
+After breakfast this morning there were only 99 drones on the tarmac. One of the
+drones never made it back from a delivery. We suspect a secret agent from Amazon
+placed an order and stole one of our patented drones. To track them down, we
+need to find their delivery ID.
 
-Given the list of IDs, which contains many duplicate integers and one unique integer, 
-find the unique integer.
+Given the list of IDs, which contains many duplicate integers and one unique
+integer, find the unique integer.
 
-The IDs are not guaranteed to be sorted or sequential. Orders aren't always fulfilled in the order they 
-were received, and some deliveries get cancelled before takeoff.
+The IDs are not guaranteed to be sorted or sequential. Orders aren't always
+fulfilled in the order they were received, and some deliveries get cancelled
+before takeoff.
 """
 
 
@@ -32,9 +36,6 @@ def test():
     assert find_missing_drone([1,2,3,4,5,1,2,3,4]) == 5
     assert find_missing_drone([21,33,21,45,78,89,45,33,89]) == 78
     assert find_missing_drone([21,33,21,45,78,89,45,33,89,78]) == 0
-    
-    
+
+
 test()
-
-
-
