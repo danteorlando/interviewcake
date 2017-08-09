53b8f1a0e4b540298186434850a964fe01b2227a
danteorlando <danteorlando@users.noreply.github.com>
2017-08-09 08:40:45 -0700

    update text

--- kth_to_last_node_in_singly_linked_list.py
+++ kth_to_last_node_in_singly_linked_list.py
@@ -1,7 +1,7 @@
 '''
 You have a linked list and want to find the kth to last node.
-Write a function kth_to_last_node() that takes an integer k and the head_node of a singly linked list, 
-and returns the kth to last node in the list.
+Write a function kth_to_last_node() that takes an integer k and the head_node of
+a singly linked list, and returns the kth to last node in the list.
 
 For example:
 
@@ -27,26 +27,30 @@ kth_to_last_node(2, a)
 
 
 Breakdown
-It might be tempting to iterate through the list until we reach the end and then walk backwards k nodes.
+It might be tempting to iterate through the list until we reach the end and then
+walk backwards k nodes.
 
 But we have a singly linked list! We can't go backwards. What else can we do?
 
 What if we had the length of the list?
 
-Then we could calculate how far to walk, starting from the head, to reach the kth to last node.
+Then we could calculate how far to walk, starting from the head, to reach the
+kth to last node.
 
 If the list has n nodes:
 
-A linked list represented by circles and arrows, with the distance from the first to the last node labeled n.
-And our target is the kth to last node:
+A linked list represented by circles and arrows, with the distance from the
+first to the last node labeled n. And our target is the kth to last node:
 
-A linked list represented by circles and arrows, with the distance from the first to the last node labeled n. 
-The third-to-last node is the kth to last node, with its distance to the last node labeled k.
-The distance from the head to the target is n-k:
+A linked list represented by circles and arrows, with the distance from the
+first to the last node labeled n. The third-to-last node is the kth to last
+node, with its distance to the last node labeled k. The distance from the head
+to the target is n-k:
 
-A linked list represented by circles and arrows, with the distance from the first to the last node labeled n. 
-The third-to-last node is the kth to last node, with its distance to the last node labeled k and its distance 
-to the first node labeled n minus k.
+A linked list represented by circles and arrows, with the distance from the
+first to the last node labeled n. The third-to-last node is the kth to last
+node, with its distance to the last node labeled k and its distance to the first
+node labeled n minus k.
 
 Well, we don't know the length of the list (n). But can we figure it out?
 
@@ -83,35 +87,43 @@ What are our time and space costs?
 
 O(n) time and O(1) space, where n is the length of the list.
 
-More precisely, it takes n steps to get the length of the list, and another n-k steps to reach the target node. 
-In the worst case k=1, so we have to walk all the way from head to tail again to reach the target node. 
-This is a total of 2n steps, which is O(n).
+More precisely, it takes n steps to get the length of the list, and another n-k
+steps to reach the target node. In the worst case k=1, so we have to walk all
+the way from head to tail again to reach the target node. This is a total of 2n
+steps, which is O(n).
 
 Can we do better?
 
 Mmmmaaaaaaybe.
 
-The fact that we walk through our whole list once just to get the length, then walk through the list again to 
-get to the kth to last element sounds like a lot of work. Perhaps we can do this in just one pass?
+The fact that we walk through our whole list once just to get the length, then
+walk through the list again to get to the kth to last element sounds like a lot
+of work. Perhaps we can do this in just one pass?
 
-What if we had like a "stick" that was k nodes wide. We could start it at the beginning of the list, so that 
-the left side of the stick was on the head and the right side was on the kth node.
+What if we had like a "stick" that was k nodes wide. We could start it at the
+beginning of the list, so that the left side of the stick was on the head and
+the right side was on the kth node.
 
-A linked list represented by circles and arrows. The third node is labeled "kth," and a linear "stick" k nodes 
-long extends from above the first node to above the kth node.
+A linked list represented by circles and arrows. The third node is labeled
+"kth," and a linear "stick" k nodes long extends from above the first node to
+above the kth node.
 
 Then we could slide the stick down the list...
 
-A linked list represented by circles and arrows. The third node is labeled "kth," and a linear "stick" k nodes 
-long extends from above the second node to above the fourth node.
+A linked list represented by circles and arrows. The third node is labeled
+"kth," and a linear "stick" k nodes long extends from above the second node to
+above the fourth node.
 
-...until the right side hit the end. At that point, the left side of the stick would be on the kth to last node!
+...until the right side hit the end. At that point, the left side of the stick
+would be on the kth to last node!
 
-A linked list represented by circles and arrows. The third-to-last node is labeled "kth to last," 
-and a linear "stick" k nodes long extends from above the kth-to-last node to above the last node.
-How can we implement this? Maybe it'll help to keep two pointers?
+A linked list represented by circles and arrows. The third-to-last node is
+labeled "kth to last," and a linear "stick" k nodes long extends from above the
+kth-to-last node to above the last node. How can we implement this? Maybe it'll
+help to keep two pointers?
 
-We can allocate two variables that'll be references to the nodes at the left and right sides of the "stick"!
+We can allocate two variables that'll be references to the nodes at the left and
+right sides of the "stick"!
 
   def kth_to_last_node(k, head):
 
@@ -142,8 +154,10 @@ We can think of this two ways.
 First approach: use the length of the list.
 
 walk down the whole list, counting nodes, to get the total list_length.
-subtract k from the list_length to get the distance from the head node to the target node (the kth to last node).
-walk that distance from the head to arrive at the target node.
+subtract k from the list_length to get the distance from the head node to the
+target node (the kth to last node). walk that distance from the head to arrive
+at the target node.
+
   def kth_to_last_node(k, head):
 
     if k < 1:
@@ -181,8 +195,10 @@ Second approach: maintain a k-wide "stick" in one walk down the list.
 
 Walk one pointer k nodes from the head. Call it right_node.
 Put another pointer at the head. Call it left_node.
-Walk both pointers, at the same speed, towards the tail. This keeps a distance of kk between them.
-When right_node hits the tail, left_node is on the target (since it's k nodes from the end of the list).
+Walk both pointers, at the same speed, towards the tail. This keeps a distance
+of kk between them. When right_node hits the tail, left_node is on the target
+(since it's k nodes from the end of the list).
+
   def kth_to_last_node(k, head):
 
     if k < 1:
@@ -214,56 +230,66 @@ When right_node hits the tail, left_node is on the target (since it's k nodes fr
     # left_node is now the kth to last node!
     return left_node
 
-In either approach, make sure to check if k is greater than the length of the linked list! 
-That's bad input, and we'll want to raise an error.
+In either approach, make sure to check if k is greater than the length of the
+linked list! That's bad input, and we'll want to raise an error.
 
 Complexity
 Both approaches use O(n) time and O(1) space.
 
-But the second approach is fewer steps, since it gets the answer "in one pass," right? Wrong.
+But the second approach is fewer steps, since it gets the answer "in one pass,"
+right? Wrong.
 
-In the first approach, we walk one pointer from head to tail (to get the list's length), 
-then walk another pointer from the head node to the target node (the kth to last node).
+In the first approach, we walk one pointer from head to tail (to get the list's
+length), then walk another pointer from the head node to the target node (the
+kth to last node).
 
-In the second approach, right_node also walks all the way from head to tail, and left_node 
-also walks from the head to the target node.
+In the second approach, right_node also walks all the way from head to tail, and
+left_node also walks from the head to the target node.
 
-So in both cases we have two pointers taking the same steps through our list. 
-The only difference is the order in which the steps are taken. The number of steps is the same either way.
+So in both cases we have two pointers taking the same steps through our list.
+The only difference is the order in which the steps are taken. The number of
+steps is the same either way.
 
-However, the second approach might still be slightly faster, due to some caching and other optimizations 
-that modern processors and memory have.
+However, the second approach might still be slightly faster, due to some caching
+and other optimizations that modern processors and memory have.
 
-Let's focus on caching. Usually when we grab some data from memory (for example, info about a linked list node), 
-we also store that data in a small cache right on the processor. If we need to use that same data again soon after, 
-we can quickly grab it from the cache. But if we don't use that data for a while, we're likely to replace it with 
-other stuff we've used more recently (this is called a "least recently used" replacement policy).
+Let's focus on caching. Usually when we grab some data from memory (for example,
+info about a linked list node), we also store that data in a small cache right
+on the processor. If we need to use that same data again soon after, we can
+quickly grab it from the cache. But if we don't use that data for a while, we're
+likely to replace it with other stuff we've used more recently (this is called a
+"least recently used" replacement policy).
 
-Both of our algorithms access a lot of nodes in our list twice, so they could exploit this caching. 
-But notice that in our second algorithm there's a much shorter time between the first and second times 
-that we access a given node (this is sometimes called "temporal locality of reference"). 
-Thus it seems more likely that our second algorithm will save time by using the processor's cache! 
-But this assumes our processor's cache uses something like a "least recently used" replacement policy 
-it might use something else. Ultimately the best way to really know which algorithm is faster is to 
-implement both and time them on a few different inputs!
+Both of our algorithms access a lot of nodes in our list twice, so they could
+exploit this caching. But notice that in our second algorithm there's a much
+shorter time between the first and second times that we access a given node
+(this is sometimes called "temporal locality of reference"). Thus it seems more
+likely that our second algorithm will save time by using the processor's cache!
+But this assumes our processor's cache uses something like a "least recently
+used" replacement policy it might use something else. Ultimately the best way to
+really know which algorithm is faster is to implement both and time them on a
+few different inputs!
 
 Bonus
-Can we do better? What if we expect n to be huge and k to be pretty small. In this case our target node will be close 
-to the end of the list...so it seems a waste that we have to walk all the way from the beginning twice.
+Can we do better? What if we expect n to be huge and k to be pretty small. In
+this case our target node will be close to the end of the list...so it seems a
+waste that we have to walk all the way from the beginning twice.
 
-Can we trim down the number of steps in the "second trip"? One pointer will certainly have to travel all the way from 
-head to tail in the list to get the total length...but can we store some "checkpoints" as we go so that the second pointer 
-doesn't have to start all the way at the beginning? Can we store these "checkpoints" in constant space? 
-Note: this approach only saves time if we know that our target node is towards the end of the list 
-(in other words, n is much larger than k).
+Can we trim down the number of steps in the "second trip"? One pointer will
+certainly have to travel all the way from head to tail in the list to get the
+total length...but can we store some "checkpoints" as we go so that the second
+pointer doesn't have to start all the way at the beginning? Can we store these
+"checkpoints" in constant space? Note: this approach only saves time if we know
+that our target node is towards the end of the list (in other words, n is much
+larger than k).
 
 What We Learned
-We listed two good solutions. One seemed to solve the problem in one pass, while the other took two passes. 
-But the single-pass approach didn't take half as many steps, it just took the same steps in a different order.
+We listed two good solutions. One seemed to solve the problem in one pass, while
+the other took two passes. But the single-pass approach didn't take half as many
+steps, it just took the same steps in a different order.
 
-So don't be fooled: "one pass" isn't always fewer steps than "two passes." 
+So don't be fooled: "one pass" isn't always fewer steps than "two passes."
 Always ask yourself, "Have I actually changed the number of steps?"
-
 
 '''
 class LinkedListNode:
@@ -278,10 +304,10 @@ def kth_to_last_node(k, head):
         list_len += 1
         node = node.next
     #print(list_len)
-     
+
     kth_to_last_idx = list_len - (k - 1)
     #print(kth_to_last_idx)
-    
+
     node = head
     for _ in xrange(1, kth_to_last_idx):
         #print(i)
@@ -302,4 +328,3 @@ d.next = e
 
 print(kth_to_last_node(3, a).value)
 # returns the node with value "Devil's Food" (the 2nd to last node)
-
