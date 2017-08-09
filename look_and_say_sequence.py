65c3593ae5552d0e53fed0e99f8e556d5289be91
danteorlando <danteorlando@users.noreply.github.com>
2017-08-09 08:07:10 -0700

    update text

--- look_and_say_sequence.py
+++ look_and_say_sequence.py
@@ -13,19 +13,20 @@ def look_and_say_sequence(current_element):
             ctr = 1
             prev = n
     l.append((ctr,prev))
-    
+
     next_element = ''
     for t in l:
         next_element += str(t[0])
         next_element += str(t[1])
-        
+
     return int(next_element)
-    
-    
-''' 
-Given is sequence of numbers: 1, 11, 21, 1211, 111221, ... 
-Find out how the sequence works and create the function to compute the next element.
-   
+
+
+'''
+Given is sequence of numbers: 1, 11, 21, 1211, 111221, ...
+Find out how the sequence works and create the function to compute the next
+element.
+
 1
 11
 21
@@ -44,9 +45,7 @@ def test():
     assert look_and_say_sequence(21) == 1211
     assert look_and_say_sequence(1211) == 111221
     assert look_and_say_sequence(111221) == 312211
-    assert look_and_say_sequence(312211) == 13112221 
+    assert look_and_say_sequence(312211) == 13112221
     assert look_and_say_sequence(13112221) == 1113213211
-    assert look_and_say_sequence(1113213211) == 31131211131221  
+    assert look_and_say_sequence(1113213211) == 31131211131221
 test()
-
-
