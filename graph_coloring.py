49afb8df41eba2c605782927082dfeaf220177b3
danteorlando <danteorlando@users.noreply.github.com>
2017-08-13 10:37:56 -0700

    added class

--- graph_coloring.py
+++ graph_coloring.py
@@ -358,3 +358,21 @@ down the final runtime, but here it actually made our runtime linear!
 
 
 '''
+
+class GraphNode:
+
+  def __init__(self, label):
+      self.label = label
+      self.neighbors = set()
+      self.color = None
+
+a = GraphNode('a')
+b = GraphNode('b')
+c = GraphNode('c')
+
+a.neighbors.add(b)
+b.neighbors.add(a)
+b.neighbors.add(c)
+c.neighbors.add(b)
+
+graph = [a, b, c]
